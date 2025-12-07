9690eaca928916f4e7b4838b7dc0c57c72f88441
Hindbeer <56268258+Hindbeer@users.noreply.github.com>
2025-12-07 20:49:22 +0300

    init project

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
-def main() -> None:
-    pass
+import asyncio
+import config
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+dp = Dispatcher()
+
+
+async def main() -> None:
+    bot = Bot(
+        token=config.BOT_TOKEN,
+        default=DefaultBotProperties(
+            parse_mode=ParseMode.HTML, disable_notification=True
+        ),
+    )
+
+    dp.include_routers()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
